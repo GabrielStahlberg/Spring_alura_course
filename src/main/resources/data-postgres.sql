74bda059b676e6cb1c8f459df5de7ecf18fba1cc
gabrielstahlberg <gabrielstahlberg@gmail.com>
2019-08-11 15:51:30 -0300

    An example project using JPA, Cache, Security, JWT token.

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,6 @@
+insert into usuario (nome, email, senha) values ('Aluno', 'aluno@gmail.com', '123456');
+
+insert into curso (nome, categoria) values ('Spring Boot', 'Programação');
+insert into curso (nome, categoria) values ('HTML 5', 'Front-end');
+
+insert into topico (titulo, mensagem, data_criacao, status, autor_id, curso_id) values ('Dúvida 1', 'Erro ao criar projeto', '2019-07-08', 'NAO_RESPONDIDO', 1, 1);
